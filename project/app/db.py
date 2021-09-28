54027a563ca20d752eef6c9d12caba8164a9207d
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-09-28 09:05:01 -0300

    chore: move database init and configure startup and shutdown

--- project/app/db.py
+++ project/app/db.py
@@ -1,13 +1,25 @@
 import os
 
+from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+
+
 TORTOISE_ORM = {
-    "connections": {
-        "default": os.environ.get("DATABASE_URL")
-    },
-        "apps": {
+    "connections": {"default": os.environ.get("DATABASE_URL")},
+    "apps": {
         "models": {
             "models": ["app.models.tortoise", "aerich.models"],
             "default_connection": "default",
         },
     },
 }
+
+
+def init_db(app: FastAPI) -> None:
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
