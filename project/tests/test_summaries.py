585a4f06df8ce9c6a7b72e249f1c394fd947b399
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-09-30 09:18:19 -0300

    test: check summaries post route

---
+++ project/tests/test_summaries.py
@@ -0,0 +1,12 @@
+import json
+
+import pytest
+
+
+def test_create_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+
+    assert response.status_code == 201
+    assert response.json()["url"] == "https://foo.bar"
