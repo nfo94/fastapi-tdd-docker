46926ddf8e62373fd0e92017a16ab27bd246d613
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-10-04 09:18:03 -0300

    test: check create summaries post with exception

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -10,3 +10,17 @@ def test_create_summary(test_app_with_db):
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
+
+
+def test_create_summaries_invalid_json(test_app):
+    response = test_app.post("/summaries/", data=json.dumps({}))
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "url"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
