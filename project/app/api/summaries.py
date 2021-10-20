bd505fb2893c1c486ff8a4b8008756f394e9feac
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-10-19 22:49:48 -0300

    chore: add get single summary router

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -16,3 +16,10 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
         "url": payload.url,
     }
     return response_object
+
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+
+    return summary
