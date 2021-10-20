8d35d9048bcc3db7956b48b0a7a7943fce645902
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-10-19 22:48:45 -0300

    test: add read summary test

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -24,3 +24,19 @@ def test_create_summaries_invalid_json(test_app):
             }
         ]
     }
+
+
+def test_read_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.get(f"/summaries/{summary_id}/")
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"]
+    assert response_dict["created_at"]
