71d769c3cbb850b7d55ed364bddb2e37275af01a
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-10-19 22:50:29 -0300

    chore: add get crud handler

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,3 +1,4 @@
+from typing import Union
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
@@ -7,3 +8,10 @@ async def post(payload: SummaryPayloadSchema) -> int:
 
     await summary.save()
     return summary.id
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    return None
