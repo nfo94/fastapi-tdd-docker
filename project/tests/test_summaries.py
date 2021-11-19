d27d37ee334aa9afeade3f5af377d2fbfa77b941
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-11-19 20:55:52 -0300

    test: check read summary with incorrect id

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -38,3 +38,9 @@ def test_read_summary(test_app_with_db):
     assert response_dict["url"] == "https://foo.bar"
     assert response_dict["summary"]
     assert response_dict["created_at"]
+
+
+def test_read_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.get("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
