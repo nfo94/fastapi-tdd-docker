f231aea76d71ebbf9ffebacd9a2f474529a6fa72
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2021-11-19 21:19:50 -0300

    chore: add get_all for summaries util function

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
 
@@ -15,3 +15,8 @@ async def get(id: int) -> Union[dict, None]:
     if summary:
         return summary[0]
     return None
+
+
+async def get_all() -> List:
+    summaries = await TextSummary.all().values()
+    return summaries
